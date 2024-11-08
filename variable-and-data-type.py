314e7358f39553f95c38b3ece847c209e62a6feb
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-11-08 23:16:26 +0600

    variable-and-data-type

---
+++ variable-and-data-type.py
@@ -0,0 +1,18 @@
+a = 5
+b = 5.6
+c = a + b
+x = 'Bangladesh'
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(x))
+print(a, b, c, x)
+
+n1 = 6
+n2 = 2
+n3 = n1 ** n2
+print(n3)
+n1 = 4
+print(n3)
+n3 = n1 ** n2
+print(n3)
